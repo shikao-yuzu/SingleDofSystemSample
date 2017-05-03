d967c03d56ce369e1a135a7700d8694455b4b98f
shikao-yuzu <shikao.yuzu@gmail.com>
2017-05-04 01:41:34 +0900

    結果追加

--- Sample.py
+++ Sample.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 from SingleDofSystem import SingleDofSystem
 
 if __name__ == "__main__":
-  acc_grd  = np.genfromtxt("acc_grd.txt", dtype=np.float64)
-  settings = json.load(open("settings.json", "r"))
+  acc_grd  = np.genfromtxt("test/nigam/acc_grd.txt", dtype=np.float64)
+  settings = json.load(open("test/nigam/settings.json", "r"))
 
   sdof = SingleDofSystem(acc_grd, **settings)
   sdof.integrate()
 
-  np.savetxt("result.csv", sdof.result(), fmt="%.10e", delimiter=",",
+  np.savetxt("test/nigam/result.csv", sdof.result(), fmt="%.10e", delimiter=",",
              header=sdof.header(), comments="")
